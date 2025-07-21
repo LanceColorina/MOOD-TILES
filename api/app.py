228d6285021e14cdf4037b0aa1484471f5037b13
Nawshhh <wilbertshawnvillagarcia@gmail.com>
2025-07-21 18:24:01 +0800

    fix: no more flask session files

--- api/app.py
+++ api/app.py
@@ -18,10 +18,6 @@ app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
 # --- Session Configuration (using filesystem) ---
 app.secret_key = os.getenv("SECRET_KEY", os.urandom(24))
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
-Session(app)
 
 # --- Spotify OAuth Setup ---
 sp_oauth = SpotifyOAuth(
